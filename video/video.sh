2177829de49fc0b748d974977c2272d33b7f7f7b
unsacrificed <unsacrificed@wwww4.ru>
2018-07-05 04:44:09 +0300

    Grammar fixes. Replace mp4 demo.

--- video/video.sh
+++ video/video.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ffmpeg -i ./site-demo-2.mov -i ./bensound-summer.mp3 -shortest -filter:a "volume=0.1,afade=t=in:ss=0:d=4,afade=t=out:st=53:d=4" -filter:v "fade=in:s=0:d=4:c=#DDDDDD,fade=out:st=53:d=4:c=#DDDDDD" -t 57 res.mp4
+ffmpeg -i ./site-demo-2.mov -i ./bensound-summer.mp3 -shortest -movflags faststart -profile:v baseline -level 3 -pix_fmt yuv420p -filter:a "volume=0.1,afade=t=in:ss=0:d=4,afade=t=out:st=53:d=4" -filter:v "fade=in:s=0:d=4:c=#DDDDDD,fade=out:st=53:d=4:c=#DDDDDD" -t 57 -vcodec libx264 res.mp4
+
 ffmpeg -i ./site-demo-2.mov -i ./bensound-summer.mp3 -shortest -filter:a "volume=0.1,afade=t=in:ss=0:d=4,afade=t=out:st=53:d=4" -filter:v "fade=in:s=0:d=4:c=#DDDDDD,fade=out:st=53:d=4:c=#DDDDDD" -t 57 -c:v libvpx-vp9 -strict experimental res.webm
